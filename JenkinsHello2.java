9fd0f573366368db83ab3e7fb72f9c7b48f2cdb5
RanaPal <47050087+RanaPal@users.noreply.github.com>
2020-09-15 01:46:31 -0400

    Add files via upload

---
+++ JenkinsHello2.java
@@ -0,0 +1,11 @@
+package BaseUtil;
+
+public class JenkinsHello2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("Hello World\nThis is a demo program");
+         System.out.println("Modified ");
+	}
+
+}
