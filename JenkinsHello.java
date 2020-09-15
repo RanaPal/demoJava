c119c68e3a18714b364a1a4c4124b59c2482d066
RanaPal <47050087+RanaPal@users.noreply.github.com>
2020-09-15 02:31:08 -0400

    Add files via upload

---
+++ JenkinsHello.java
@@ -0,0 +1,8 @@
+public class JenkinsHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("Hello World\nThis is a demo program");
+	}
+
+}
