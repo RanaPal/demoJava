6e91c5b6fd00302903399e45f36430812cd03ada
apal <achintya_pal@comcast.net>
2022-08-26 01:31:43 -0400

    update

--- JenkinsHello.java
+++ JenkinsHello.java
@@ -1,8 +1,8 @@
+
 public class JenkinsHello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-         System.out.println("Hello World\nThis is a demo program");
-	}
-
+         System.out.println("Hello World\nThis is a demo program")}
+	system.out.println("abcd");
 }
